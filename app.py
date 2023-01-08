7361e6cf676ba36145b3d70888eec79181e76a9b
FeckNeck <math.dousse@hotmail.fr>
2023-01-08 20:19:37 +0100

    final

--- app.py
+++ app.py
@@ -1,34 +1,33 @@
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
 import dash
 from dash import Dash, html, dcc
 
 # ----- # - Create Corpus file - # ----- #
 
-from modules.Corpus import Corpus
-from modules.Api import Api
-
-api = Api()
-api.fetchReddit()
-api.fetchArxiv()
-
-dicDoc = api.getDicDoc()
-dicAuth = api.getDicAuthor()
-
-corpus = Corpus('Corpus Arxiv/Reddit', dicAuth,
-                dicDoc, len(dicDoc), len(dicAuth))
-
-api.saveCorpus(corpus)
-
-# ----- # - Create WordCloud - # ----- #
-
-words = corpus.clean_doc()
-text = ' '.join(words)
-
-wordcloud = WordCloud(background_color='white', max_words=50).generate(text)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.savefig('assets/images/wordCloud.png')
+#from modules.Corpus import Corpus
+#from modules.Api import Api
+#
+#api = Api()
+#api.fetchReddit()
+#api.fetchArxiv()
+#
+#dicDoc = api.getDicDoc()
+#dicAuth = api.getDicAuthor()
+#
+#corpus = Corpus('Corpus Arxiv/Reddit', dicAuth,
+#                dicDoc, len(dicDoc), len(dicAuth))
+#
+#api.saveCorpus(corpus)
+#
+## ----- # - Create WordCloud - # ----- #
+#import matplotlib.pyplot as plt
+#from wordcloud import WordCloud
+#words = corpus.clean_doc()
+#text = ' '.join(words)
+#
+#wordcloud = WordCloud(background_color='white', max_words=50).generate(text)
+#plt.imshow(wordcloud)
+#plt.axis("off")
+#plt.savefig('assets/images/wordCloud.png')
 
 
 app = Dash(__name__, suppress_callback_exceptions=True, use_pages=True)
